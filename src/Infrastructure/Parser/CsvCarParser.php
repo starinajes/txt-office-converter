df653a3c20e2ec9db15fa3a5f4fd74e4c78a5aaa
Ivan Y <jesters@inbox.ru>
2025-07-08 17:27:28 +0300

    fix: car parser

--- src/Infrastructure/Parser/CsvCarParser.php
+++ src/Infrastructure/Parser/CsvCarParser.php
@@ -4,6 +4,7 @@ namespace App\Infrastructure\Parser;
 use App\Domain\Common\Parser\ParserInterface;
 use App\Domain\Common\Entity\EntityInterface;
 use App\Domain\Car\Entity\Car;
+use RuntimeException;
 
 class CsvCarParser implements ParserInterface
 {
@@ -14,16 +15,26 @@ class CsvCarParser implements ParserInterface
     public function parse(string $filePath): array
     {
         $handle = fopen($filePath, 'r');
+
         if ($handle === false) {
-            throw new \RuntimeException("Не удалось открыть файл: $filePath");
+            throw new RuntimeException("Не удалось открыть файл: $filePath");
         }
+
         $cars = [];
-        $header = fgetcsv($handle, 0, ",", "\"", "\\");
-        while (($row = fgetcsv($handle, 0, ",", "\"", "\\")) !== false) {
+        fgetcsv($handle, 0);
+
+        while (($row = fgetcsv($handle, 0)) !== false) {
             [$id, $brand, $model, $year] = $row;
-            $cars[] = new Car((int)$id, $brand, $model, (int)$year);
+
+            $cars[] = new Car(
+                id: (int)$id,
+                brand: $brand,
+                model: $model,
+                year: (int)$year);
         }
+
         fclose($handle);
+
         return $cars;
     }
 } 
